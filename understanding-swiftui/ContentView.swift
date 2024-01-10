a12b35056ab0a6c203b8725abbd6c404bea29248
acastanedadev <acastaneda@enerser.com.mx>
2024-01-09 17:03:44 -0800

    Initial Commit

---
+++ understanding-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  understanding-swiftui
+//
+//  Created by Desarrollo on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ understanding-swiftui/understanding_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  understanding_swiftuiApp.swift
+//  understanding-swiftui
+//
+//  Created by Desarrollo on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct understanding_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
