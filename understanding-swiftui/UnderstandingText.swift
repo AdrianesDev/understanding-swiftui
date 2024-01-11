b29aafd786925ed35debbc1ef718ec18398c8fe8
acastanedadev <acastaneda@enerser.com.mx>
2024-01-10 17:25:05 -0800

    text

---
+++ understanding-swiftui/UnderstandingText.swift
@@ -0,0 +1,18 @@
+//
+//  UnderstandingText.swift
+//  understanding-swiftui
+//
+//  Created by Desarrollo on 10/01/24.
+//
+
+import SwiftUI
+
+struct UnderstandingText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnderstandingText()
+}
