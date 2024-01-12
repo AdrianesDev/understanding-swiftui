d53d6c089b80c6f1bd46c44dcbcf4dd2c1de4cf2
acastanedadev <acastaneda@enerser.com.mx>
2024-01-12 12:04:56 -0800

    Text part 23

--- understanding-swiftui/UnderstandingText.swift
+++ understanding-swiftui/UnderstandingText.swift
@@ -8,16 +8,21 @@
 import SwiftUI
 
 struct UnderstandingText: View {
+   
     var body: some View {
         VStack(spacing: 15) {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .font(
                     .system(size: 40,
-                            weight: .ultraLight,
+                            weight: .light,
                             design: .default
                            )
                 )
-            Text("Hello")
+                .frame(width: 300,height: 100,alignment: .topTrailing)
+                .background(Color.green)
+                .foregroundColor(.green)
+                .opacity(0.4)
+            Text("Esta es una practica multilinea")
                 .font(
                     .system(size: 40,
                             weight: .regular,
@@ -25,23 +30,43 @@ struct UnderstandingText: View {
                            )
                 )
                 .lineLimit(2)
+                .frame(height: 100)
                 .truncationMode(.tail)
+                .padding([.leading,.bottom],50)
                 .background(Color.gray)
                 .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                
+                
             
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .font(
                     .custom("Futura-Medium", size: 50, relativeTo: .title)
                 )
                 .frame(height: 70)
+                .padding(.leading,50)
                 .foregroundStyle(.white)
                 .background(Color.black)
+                
+            
+            
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .font(.largeTitle)
                 //.fontWeight(.black)
                 .underline(true,color: .blue)
                 .italic()
                 .bold()
+                .frame(height: 100)
+                .border(.orange,width: 7)
+                .kerning(10) // leterspacing
+            
+            Text("Hello,Developer")
+                .font(.system(size: 100,weight: .light))
+                .bold()
+                .minimumScaleFactor(0.7)
+                .lineLimit(1)
+            
+            
         }
     }
 }
