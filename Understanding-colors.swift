38a4177cf6e15a366d5924000e4eb15c30c405cc
acastanedadev <acastaneda@enerser.com.mx>
2024-01-17 17:22:29 -0800

    new pages

--- Understanding-colors.swift
+++ Understanding-colors.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  Understanding-colors.swift
 //  understanding-swiftui
 //
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct Understanding_colors: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Color(
+                red: 112/255,
+                green: 160/255,
+                blue: 143/255,
+                opacity: 1)
+                .frame(width: 300,height: 100)
+            .clipShape(RoundedRectangle(cornerRadius: 40))
+            .shadow(
+                color: .black,
+                radius:0,
+            x: 8,
+            y: 7)
+            
+            Color(
+                hue:0.2,
+                saturation: 0.9,
+                brightness: 0.5
+            )
+            .frame(
+                width: 200,
+                height: 100
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius:0,
+            x: 8,
+            y: 7)
+            
+        }
     }
+        
 }
 
 #Preview {
--- Understanding-rectangle.swift
+++ Understanding-rectangle.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct Understanding_rectangle: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 20) {
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 300,height: 200)
+                .clipShape(.rect(cornerRadius:20))
+                
+                RoundedRectangle(cornerRadius: 55,style: .continuous)
+                    .fill(.green)
+                    .frame(width: 300,height: 200)
+                RoundedRectangle(cornerRadius: 55,style: .circular)
+                    .fill(.green)
+                    .frame(width: 300,height: 200)
+                Capsule()
+                    .fill(.red)
+                    .frame(width: 200,height: 100)
+                
+                Ellipse()
+                    .fill(.brown)
+                    .frame(width: 200,height: 100)
+                
+                Circle()
+                    .fill(.cyan)
+                    .frame(width: 200,height: 200)
+            }
+        }
     }
 }
 
