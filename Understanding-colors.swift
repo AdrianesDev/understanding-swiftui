7d7e8bf63da027af7944a7ede3dae908120135b4
acastanedadev <acastaneda@enerser.com.mx>
2024-01-17 17:21:28 -0800

    new pages

---
+++ Understanding-colors.swift
@@ -0,0 +1,18 @@
+//
+//  Understanding-colors.swift
+//  understanding-swiftui
+//
+//  Created by Desarrollo on 17/01/24.
+//
+
+import SwiftUI
+
+struct Understanding_colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Understanding_colors()
+}
---
+++ Understanding-rectangle.swift
@@ -0,0 +1,18 @@
+//
+//  Understanding-rectangle.swift
+//  understanding-swiftui
+//
+//  Created by Desarrollo on 17/01/24.
+//
+
+import SwiftUI
+
+struct Understanding_rectangle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Understanding_rectangle()
+}
