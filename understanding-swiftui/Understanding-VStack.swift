923ce76e19326448cbb6ba6c35924a4f19605053
acastanedadev <acastaneda@enerser.com.mx>
2024-01-18 17:27:24 -0800

    new understanding pages

---
+++ understanding-swiftui/Understanding-VStack.swift
@@ -0,0 +1,18 @@
+//
+//  Understanding-VStack.swift
+//  understanding-swiftui
+//
+//  Created by Desarrollo on 18/01/24.
+//
+
+import SwiftUI
+
+struct Understanding_VStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Understanding_VStack()
+}
