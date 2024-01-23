1699ab6f44f0bbac981a5381080feb5e3c12544a
acastanedadev <acastaneda@enerser.com.mx>
2024-01-22 17:29:01 -0800

    understanding VStack

--- understanding-swiftui/Understanding-VStack.swift
+++ understanding-swiftui/Understanding-VStack.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct Understanding_VStack: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello")
+                .font(.system(
+                    size: 50,
+                    weight: .bold
+            ))
+            Text("Developer")
+                .font(.system(size: 40,
+                              weight: .medium
+                             ))
+            Text("What do you want learn today?")
+                .font(.system(size: 30,
+                              weight: .medium
+                             ))
+            Color.teal
+                .frame(width: 300,height: 200)
+                .clipShape(.rect(cornerRadius:10))
+                
+        }
+        .frame(width: 400)
+        .background(.gray)
+            
+       
     }
 }
 
